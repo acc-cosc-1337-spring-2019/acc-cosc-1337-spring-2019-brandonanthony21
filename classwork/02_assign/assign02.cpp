ab096960eba8cf6a7511ea412eda55be4d7392c2
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2019-01-30 19:07:33 -0600

    missing function definition

--- classwork/02_assign/assign02.cpp
+++ classwork/02_assign/assign02.cpp
@@ -1,4 +1,7 @@
 //Write the function code that returns the product of hours and hourly_rate.
 #include "assign02.h"
 
-double gross_pay(double hours, double hourly_rate);
+double gross_pay(double hours, double hourly_rate)
+{
+  return hours * hourly_rate;
+}
